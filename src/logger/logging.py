8a3028cf995f870c1e5482a7a1b38ab713e9ad0c
VigneshwarKandhaiya1207 <vigneshwar.k2@gmail.com>
2024-06-24 12:10:26 +0530

    Logging and exceptions added

---
+++ src/logger/logging.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
+
+log_file_path=os.path.join(os.getcwd(),"logs")
+
+if not os.path.exists(log_file_path):
+    try:
+        os.makedirs(log_file_path,exist_ok=True)
+    except Exception as e:
+        pass
+else:
+    print("The directoy already exists.")
+
+LOG_FILE_NAME=os.path.join(log_file_path,LOG_FILE)
+
+logging.basicConfig(level=logging.INFO,
+                    filename=LOG_FILE_NAME,
+                    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s")
